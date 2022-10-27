0d1922a44fa37a74030f22727e88702dd01863eb
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2022-10-27 15:56:09 -0300

    add calculater n prime number

--- SwiftUIManagerState.playground/Contents.swift
+++ SwiftUIManagerState.playground/Contents.swift
@@ -1,5 +1,101 @@
 import SwiftUI
 
+public let wolframAlphaApiKey = "6H69Q3-828TKQJ4EP"
+
+struct WolframAlphaResult: Decodable {
+    let queryresult: QueryResult
+
+    struct QueryResult: Decodable {
+        let pods: [Pod]
+
+        struct Pod: Decodable {
+            let primary: Bool?
+            let subpods: [SubPod]
+
+            struct SubPod: Decodable {
+                let plaintext: String
+            }
+        }
+    }
+}
+
+func wolframAlpha(query: String, callback: @escaping (WolframAlphaResult?) -> Void) -> Void {
+    var components = URLComponents(string: "https://api.wolframalpha.com/v2/query")!
+    components.queryItems = [
+        URLQueryItem(name: "input", value: query),
+        URLQueryItem(name: "format", value: "plaintext"),
+        URLQueryItem(name: "output", value: "JSON"),
+        URLQueryItem(name: "appid", value: wolframAlphaApiKey),
+    ]
+
+    URLSession.shared.dataTask(with: components.url(relativeTo: nil)!) { data, response, error in
+        callback(
+            data
+                .flatMap { try? JSONDecoder().decode(WolframAlphaResult.self, from: $0) }
+        )
+    }
+    .resume()
+}
+
+
+func nthPrime(_ n: Int, callback: @escaping (Int?) -> Void) -> Void {
+    wolframAlpha(query: "prime \(n)") { result in
+        callback(
+            result
+                .flatMap {
+                    $0.queryresult
+                        .pods
+                        .first(where: { $0.primary == .some(true) })?
+                        .subpods
+                        .first?
+                        .plaintext
+                }
+                .flatMap(Int.init)
+        )
+    }
+}
+
+//nthPrime(1_000) { p in
+//  print(p)
+//}
+
+
+struct IsPrimeModalView: View {
+    @ObservedObject var state: AppState
+    var body: some View {
+        VStack {
+            if isPrime(self.state.count) {
+                Text("\(self.state.count) is prime 🎉")
+                if self.state.favoritePrimes.contains(self.state.count) {
+                    Button(action: {
+                        self.state.favoritePrimes.removeAll(where: { $0 == self.state.count })
+                    }) {
+                        Text("Remove from favorite primes")
+                    }
+                } else {
+                    Button(action: {
+                        self.state.favoritePrimes.append(self.state.count)
+                    }) {
+                        Text("Save to favorite primes")
+                    }
+                }
+            } else {
+                Text("\(self.state.count) is not prime :(")
+            }
+
+        }
+    }
+
+    private func isPrime (_ p: Int) -> Bool {
+        if p <= 1 { return false }
+        if p <= 3 { return true }
+        for i in 2...Int(sqrtf(Float(p))) {
+            if p % i == 0 { return false }
+        }
+        return true
+    }
+}
+
 struct ContentView: View {
     @ObservedObject var state: AppState
 
@@ -30,27 +126,29 @@ import Combine
 
 class AppState: ObservableObject {
     @Published var count = 0
-//    var count = 0 {
-//        willSet {
-//            self.objectWillChange.send()
-//        }
-//    }
+    @Published var favoritePrimes: [Int] = []
+}
 
-//    var didChange = PassthroughSubject<Void, Never>()
+struct PrimeAlert: Identifiable {
+  let prime: Int
+
+  var id: Int { self.prime }
 }
 
 struct CounterView: View {
     @ObservedObject var state: AppState
+    @State var isPrimeModalShown: Bool = false
+    @State var alertNthPrime: PrimeAlert?
+    @State var isNthPrimeButtonDisabled = false
 
     var body: some View {
-//        self.$count // Binding<Int>
         VStack {
             HStack {
                 Button(action: {
                     self.state.count -= 1
                 }) {
                     Text("-")
-                    }
+                }
                 Text("\(self.state.count)")
                 Button(action: {
                     self.state.count += 1
@@ -58,16 +156,33 @@ struct CounterView: View {
                     Text("+")
                 }
             }
-            Button(action: {}) {
+            Button(action: { self.isPrimeModalShown = true }) {
                 Text("Is this prime?")
             }
-            Button(action: {}) {
+            Button(action: self.nthPrimeButtonAction) {
                 Text("What is the \(ordinal(self.state.count)) prime?")
             }
         }
         .font(.title)
         .navigationTitle("Counter Demo")
+        .sheet(isPresented: self.$isPrimeModalShown){
+            IsPrimeModalView(state: self.state)
+        }
+        .alert(item: self.$alertNthPrime) { alert in
+            Alert(
+                title: Text("The \(ordinal(self.state.count)) prime is \(alert.prime)"),
+                dismissButton: .default(Text("Ok"))
+            )
+        }
     }
+
+    func nthPrimeButtonAction() {
+        self.isNthPrimeButtonDisabled = true
+        nthPrime(self.state.count) { prime in
+          self.alertNthPrime = prime.map(PrimeAlert.init(prime:))
+          self.isNthPrimeButtonDisabled = false
+        }
+      }
 }
 
 import PlaygroundSupport
